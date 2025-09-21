da0c5fd321e160bda9665497472cad26bcaffc31
siepk <siepkakamil@gmail.com>
2025-09-21 11:28:28 +0200

    Init

---
+++ src/main/java/com/larpologic/secretnetwork/SecretNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.larpologic.secretnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/larpologic/secretnetwork/SecretNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larpologic.secretnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
